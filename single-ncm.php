e89a2e4e9344493d13ba811fa1dfdc33f6ab266d
Eduardo Almeida <eduardoalmeida258@gmail.com>
2017-05-04 17:52:35 -0300

    04.05 - Change View Cursos

--- single-ncm.php
+++ single-ncm.php
@@ -9,11 +9,14 @@
 		<header>
 			<h3><?php the_title() ?></h3>
 		</header>
-		<main>
-		<hr>
-                <h4 class="title"><?php echo getHierarquiaTitulo(get_the_ID()); ?></h4>
-		<hr>
+		<p>
+		<?php
 
+			$categorias = get_the_category();
+			echo json_encode($categorias);
+		?>
+		</p>
+		<main>
 			<?php $observacao_id = get_post_meta(get_the_ID(), 'observacao')[0] ?>
 			<?php $observacao = get_post($observacao_id); ?>
 			<?php conteudo($observacao->post_content); ?>
